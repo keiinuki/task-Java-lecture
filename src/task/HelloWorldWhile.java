627a4f76f59dacb58e5c8944e2b04be8ebc2a1b8
keiinuki <k.inuki@haguruma.co.jp>
2022-05-08 07:35:47 +0900

    第一回講義の課題提出

---
+++ src/task/HelloWorldWhile.java
@@ -0,0 +1,15 @@
+package task;
+
+public class HelloWorldWhile {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int i = 1;
+        while(i <= 10){
+        	System.out.println("Hello World " + i);
+        	i = i + 1;
+        }
+        System.out.println("Hello!!" + 11);
+	}
+
+}
