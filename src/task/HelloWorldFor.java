93a4d18c75bba8b3f2c15f31f965a3936c10cad9
keiinuki <k.inuki@haguruma.co.jp>
2022-05-08 16:03:54 +0900

    レビュー指摘箇所の修正

--- src/task/HelloWorldFor.java
+++ src/task/HelloWorldFor.java
@@ -5,9 +5,9 @@ public class HelloWorldFor {
 	public static void main(String[] args) {
 		for (int i = 1; i < 12; i++) {
 			if (i < 11) {
-				System.out.println("Hello World " + i + " 回目");
+				System.out.println("Hello World " + i + "回目");
 			} else {
-				System.out.println("Hello!! " + i + " 回目");
+				System.out.println("Hello!! " + i + "回目");
 			}
 		}
 	}
