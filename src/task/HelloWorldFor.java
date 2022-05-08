4e7db027b533416592a5db8e05d5c0cb5d78fe1a
keiinuki <k.inuki@haguruma.co.jp>
2022-05-08 15:38:34 +0900

    不要なコメント削除＋フォーマット＋スペース位置変更

--- src/task/HelloWorldFor.java
+++ src/task/HelloWorldFor.java
@@ -3,14 +3,13 @@ package task;
 public class HelloWorldFor {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		for (int i = 1; i < 12; i++){
-        	if(i < 11) {
-        		System.out.println("Hello World " + i + " 回目");
-        	} else {
-        System.out.println("Hello!!" + i + " 回目");
-        	}
-	    }
+		for (int i = 1; i < 12; i++) {
+			if (i < 11) {
+				System.out.println("Hello World " + i + " 回目");
+			} else {
+				System.out.println("Hello!! " + i + " 回目");
+			}
+		}
 	}
 
 }
